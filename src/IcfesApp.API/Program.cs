30106609668d1aaef6c5d3d1d6f43a5c0980fb9f
AlejandroGiron97 <wialgica@gmail.com>
2026-08-04 12:37:00 -0500

    config CORS

--- src/IcfesApp.API/Program.cs
+++ src/IcfesApp.API/Program.cs
@@ -10,12 +10,29 @@ builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
+const string LocalDevCorsPolicy = "LocalDevCorsPolicy";
+if (builder.Environment.IsDevelopment())
+{
+    // Solo en Development: permite cualquier origen localhost/127.0.0.1 (cualquier puerto)
+    // para no bloquear a los equipos de front (Angular, React, etc.) mientras prueban.
+    builder.Services.AddCors(options =>
+    {
+        options.AddPolicy(LocalDevCorsPolicy, policy =>
+            policy.SetIsOriginAllowed(origin =>
+                    Uri.TryCreate(origin, UriKind.Absolute, out var uri) &&
+                    (uri.Host == "localhost" || uri.Host == "127.0.0.1"))
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+    });
+}
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    app.UseCors(LocalDevCorsPolicy);
 }
 
 app.UseHttpsRedirection();
